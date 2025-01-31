5ee850a2c3375f280b5c50d26de9c0a0c71cb8ff
photon256 <144157217+photon256@users.noreply.github.com>
2025-01-31 06:56:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', 22801364''))
+API_ID = int(environ.get('API_ID', '22801364'))
 API_HASH = environ.get('API_HASH', '38fd13fcb81f612aa703fa63d7dffec8')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
